7d82b631d5800712e45385fc1f12986accda43dc
Alexander Sakharov <sakharov487@mail.ru>
2021-05-03 16:40:59 +0300

    Added availability status entity

---
+++ ProCat-backend/src/main/java/proCat/entity/AvailabilityStatus.java
@@ -0,0 +1,20 @@
+package proCat.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "availability_status")
+public class AvailabilityStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "status_id", nullable = false)
+    private Long statusId;
+    @Column(name = "status_name", nullable = false)
+    private String statusName;
+    @OneToMany(mappedBy = "availabilityStatus")
+    private Set<RentInventory> inventorySet;
+}
