84aa23c9cf21e9e4e60242ab3340ca9c6f43fbcf
Alexander Sakharov <sakharov487@mail.ru>
2021-05-04 15:30:53 +0300

    Added rent status repository

---
+++ ProCat-backend/src/main/java/proCat/repository/RentStatusRepository.java
@@ -0,0 +1,9 @@
+package proCat.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import proCat.entity.RentStatus;
+
+@Repository
+public interface RentStatusRepository extends JpaRepository<RentStatus, Long> {
+}
