2ebf66c3048e7baf6adb5279407e2d7dc986af20
Pavel Mikrasov <mikrasov.p@gmail.com>
2021-05-19 17:42:18 +0300

    Added profile and auth screen layout

--- ProCat-android/app/src/main/java/vsu/csf/procat/ui/profile/ProfileActivity.kt
+++ ProCat-android/app/src/main/java/vsu/csf/procat/ui/profile/ProfileActivity.kt
@@ -1,12 +1,14 @@
-package vsu.csf.procat.ui
+package vsu.csf.procat.ui.profile
 
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import timber.log.Timber
 import vsu.csf.procat.R
 import vsu.csf.procat.databinding.ActivityProfileBinding
+import vsu.csf.procat.ui.auth.AuthActivity
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -16,6 +18,17 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_profile)
+        binding.activity = this
+
+        setSupportActionBar(binding.profileToolbar)
+    }
+
+    fun openLoginActivity() {
+        AuthActivity.startForLogin(this)
+    }
+
+    fun openRegistrationActivity() {
+        AuthActivity.startForRegistration(this)
     }
 
     companion object {
