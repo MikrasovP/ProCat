8d7ec364a06891fbf2fb122efb84114c1cda7150
Alexander Sakharov <sakharov487@mail.ru>
2021-06-12 18:36:09 +0300

    Added response status

--- ProCat-backend/src/main/java/proCat/exception/UserNotFoundException.java
+++ ProCat-backend/src/main/java/proCat/exception/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package proCat.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UserNotFoundException extends Exception {
     public UserNotFoundException(String message) {
         super(message);
