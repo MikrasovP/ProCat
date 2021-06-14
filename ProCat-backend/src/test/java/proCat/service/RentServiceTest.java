b8c501c32c2bef5a9b7792a77743c7ef3a22db59
Alexander Sakharov <sakharov487@mail.ru>
2021-06-14 20:06:53 +0300

    Added test for rent service

---
+++ ProCat-backend/src/test/java/proCat/service/RentServiceTest.java
@@ -0,0 +1,43 @@
+package proCat.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import proCat.entity.Rent;
+import proCat.exception.PaymentTimeOutException;
+import proCat.repository.AvailabilityStatusRepository;
+import proCat.repository.RentInventoryRepository;
+import proCat.repository.RentRepository;
+import proCat.repository.UserRepository;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RentServiceTest {
+    @InjectMocks
+    private RentService rentService;
+
+    @Mock
+    private RentInventoryRepository inventoryRepository;
+    @Mock
+    private RentRepository rentRepository;
+
+    @Mock
+    private AvailabilityStatusRepository availabilityStatusRepository;
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    public void payForRentTimeOut() {
+        Rent rent = new Rent();
+        rent.setRentId(1L);
+        rent.setEndTime(LocalDateTime.now().plusMinutes(12).plusSeconds(1));
+        Mockito.doReturn(rent).when(rentRepository).getByRentId(1L);
+        assertThrows(PaymentTimeOutException.class,()->{rentService.payForRent(1L);});
+    }
+}
