f46dfb189e1809f8ee46c41df18728341896b32d
Alexander Sakharov <sakharov487@mail.ru>
2021-06-16 20:53:04 +0300

    Fixed a bug with incorrect identification of the inventory user

--- ProCat-backend/src/main/java/proCat/repository/RentRepository.java
+++ ProCat-backend/src/main/java/proCat/repository/RentRepository.java
@@ -10,12 +10,13 @@ import java.util.UUID;
 
 @Repository
 public interface RentRepository extends JpaRepository<Rent, Long> {
-    @Query(value = "select case when count(r) > 0 then true else false end from rent r where r.user_id=:user_id and inventory_id=:inv_id and status_id = 1", nativeQuery = true)
+    @Query(value = "select case when count(r) > 0 then true else false end from rent r where r.user_id=:user_id and inventory_id=:inv_id and status_id in(1,2)", nativeQuery = true)
     boolean isInventoryRentedByUser(@Param("user_id") Long userId,
                                     @Param("inv_id") UUID inventoryId);
 
-    @Query(value = "select r.* from rent r where r.user_id=:user_id and inventory_id=:inv_id and status_id = 1 ",nativeQuery = true)
+    @Query(value = "select r.* from rent r where r.user_id=:user_id and inventory_id=:inv_id and status_id in(1,2) ", nativeQuery = true)
     Rent getCurrentUserRent(@Param("user_id") Long userId,
                             @Param("inv_id") UUID inventoryId);
+
     Rent getByRentId(Long id);
 }
